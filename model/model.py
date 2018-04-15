91dfd6e3e6bd7d7f9d6d94eea08232e02409e19d
zhao0189 <zzn0189@gmail.com>
2018-04-15 17:26:07 -0400

    Pretrain Model Completed

--- model/model.py
+++ model/model.py
@@ -10,15 +10,20 @@ from torch.optim.lr_scheduler import StepLR
 """
 Problems:
 0. How to represent each data? Transform the full length of wav into a matrix or just part of it or maybe fix length of it?
-1. How to make sure that outputs from CNN are all same sized?
+try different lengths.
 2. How to select triplets (Anchor, Pos and Neg)?
+randome choice.
 3. How to iterate three dataloaders?
+triplet loader
+
+pretrain loss fn: weighted cross entropy
 """
 
 
 class CNN(torch.nn.Module):
-    def __init__(self):
-        super(CNN).__init__()
+    def __init__(self, num_of_class):
+        super().__init__()
+        self.num_of_class = num_of_class
 
         self.conv1 = torch.nn.Conv1d(40, 96, 3, 1, 1)
         self.relu1 = torch.nn.LeakyReLU()
@@ -72,7 +77,7 @@ class CNN(torch.nn.Module):
         self.bn6 = torch.nn.BatchNorm1d(192)
         self.bn7 = torch.nn.BatchNorm1d(384)
         self.bn8 = torch.nn.BatchNorm1d(384)
-        self.bn9 = torch.nn.BatchNorm1d(384)
+        self.bn9 = torch.nn.BatchNorm1d(384)      # TODO: change back to 384
         self.bn10 = torch.nn.BatchNorm1d(384)
         self.bn11 = torch.nn.BatchNorm1d(384)
         self.bn12 = torch.nn.BatchNorm1d(192)
@@ -84,13 +89,11 @@ class CNN(torch.nn.Module):
         self.bn18 = torch.nn.BatchNorm1d(46)
 
         self.pooling = AvgPool()
-
         # MLP layer
-        # TODO: change linear layer dimension. Pad input data at the begging or fix in put data dimension?
-        self.linear = torch.nn.Linear(28 * 28, 500)
+        self.linear = torch.nn.Linear(384, self.num_of_class)
         self.mlp = torch.nn.LeakyReLU()
 
-    def forward(self, input_val, onehot, num_frames, pre_train=True):
+    def forward(self, input_val, pre_train=True):
         input_val = input_val.transpose_(1, 2)
 
         conv1 = self.conv1(input_val)
@@ -108,7 +111,7 @@ class CNN(torch.nn.Module):
 
         part_three = self.relu9(self.bn9(self.conv9(
             self.relu8(self.bn8(self.conv8(self.relu7(self.bn7(self.conv7(part_two)))))))))
-
+        """
         part_four = self.relu12(self.bn12(self.conv12
             (self.relu11(self.bn11(self.conv11(self.relu10(self.bn10(self.conv10(part_three)))))))))
 
@@ -117,10 +120,10 @@ class CNN(torch.nn.Module):
 
         part_six = self.relu18(self.bn18(self.conv18
             (self.relu17(self.bn17(self.conv17(self.relu16(self.bn16(self.conv16(part_five)))))))))
+        """
+        conv_out = part_three     # TODO: change back to part_six
 
-        conv_out = part_six
-
-        pool = self.pooling(conv_out, onehot, num_frames)
+        pool = self.pooling(conv_out)
 
         if pre_train:
             return self.mlp(self.linear(pool))
@@ -130,33 +133,44 @@ class CNN(torch.nn.Module):
 
 class AvgPool(torch.nn.Module):
 
-    def forward(self, conv_out, onehot, num_frames):
-        onehot = onehot.transpose_(1, 2).squeeze()
-        conv_out = conv_out.transpose_(1, 2).squeeze()
-        num_frames = num_frames.transpose_(0, 1)
-        res = torch.mm(onehot, conv_out)
-        res = res / num_frames
+    def forward(self, conv_out):
+        res = torch.mean(conv_out, dim=2)
         return res
 
 
 class MyDataset(Dataset):
     def __init__(self, dir):
+        self.dir = dir
         self.data_files = os.listdir(dir)  # loads a list of files in __init__
-        # TODO: sort data files to make sure training data are aligned?
+        # Select only numpy files
+        self.data_files = [file for file in self.data_files if file.endswith(".npy")]
 
-    def __getindex__(self, idx):
-        return load_file(self.data_files[idx])
+        # Get total number of classes and save into a dictionary
+        cnt = 0
+        self.label_dict = {}
+        for data_file in self.data_files:
+            person = data_file.split("-")[0]
+            if person not in self.label_dict:
+                self.label_dict[person] = cnt
+                cnt += 1
+        self.total_labels = len(self.label_dict)
+
+    def __getitem__(self, item):
+        # Get training data
+        filename = self.data_files[item]
+        print(filename)
+        X = np.load(self.dir+filename)
+
+        # Build data label one-hot vector
+        person = filename.split("-")[0]
+        idx = np.array([self.label_dict[person]])
+        # Y = np.zeros([self.total_labels], dtype=float)
+        # Y[idx] = 1
+        print(idx)
+        return to_tensor(X), to_tensor(idx)
 
     def __len__(self):
         return len(self.data_files)
-
-
-def load_file(filename):
-    """
-    TODO: load data matrix and return matrix and its label
-    open file, load data, and load label
-    """
-    return
 
 
 def init_xavier(m):
@@ -184,16 +198,63 @@ def expand_labels(labels):
     return b
 
 
-def collate():
-    return
-
-
 if __name__ == "__main__":
     batch_size = 1
-    dataset1 = MyDataset("DIR")
-    loader1 = torch.utils.data.DataLoader(dataset1, shuffle=False, batch_size=batch_size, collate_fn=collate)
+    lr = 0.001
+    epochs = 20
+    num_of_class = 3    # TODO: change to REAL number of classes
+    pretrain = True
 
-    # TODO: iterate data
-    loss_fn = torch.nn.TripletMarginLoss(margin=1.0, p=2)
-    # loss = loss_fn(input1, input2, input3) #anchor, positive, negative
-    model = CNN()
+    if pretrain:
+        model = CNN(num_of_class)   # initialize the model
+        model.apply(init_xavier)
+
+        """ Pretrain Process -- train on single files """
+        dir = os.path.dirname(os.path.abspath(__file__))
+        dir = os.path.join(os.path.dirname(dir), "data/")   # directory of single training instances
+        pretrain_dataset = MyDataset(dir)
+
+        # Currently batch size set to 1. Padding required for >1 batch size.
+        pretrain_loader = torch.utils.data.DataLoader(pretrain_dataset, batch_size=batch_size, shuffle=True)
+
+        loss_fn = torch.nn.CrossEntropyLoss(size_average=False)
+        optim = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.8, weight_decay=0.001)
+        scheduler = StepLR(optim, step_size=3, gamma=0.8)
+
+        if torch.cuda.is_available():
+            # Move the network and the optimizer to the GPU
+            model = model.cuda()
+            loss_fn = loss_fn.cuda()
+
+        for epoch in range(epochs):
+            print("Epoch: " + str(epoch))
+            losses = []
+            counter = 1
+            scheduler.step()
+            for (input_val, label) in pretrain_loader:
+
+                # TBC
+                if counter % 20 == 0:
+                    optim.step()
+                    optim.zero_grad()
+
+                prediction = model(to_variable(input_val))
+                label = label.transpose_(0, 1).long().resize_(batch_size)
+
+                loss = loss_fn(prediction, to_variable(label))
+                loss.backward()
+                losses.append(loss.data.cpu().numpy())
+
+                if counter % 1 == 0:
+                    print(loss)
+                counter += 1
+
+            print("Epoch {} Loss: {:.4f}".format(epoch, np.asscalar(np.mean(losses))))
+
+    else:
+        # Training Process -- train on trials
+        # TODO: training process
+        dataset1 = MyDataset("DIR")
+        loader1 = torch.utils.data.DataLoader(dataset1, shuffle=False, batch_size=batch_size)
+        loss_fn = torch.nn.TripletMarginLoss(margin=1.0, p=2)
+        # loss = loss_fn(input1, input2, input3) #anchor, positive, negative
