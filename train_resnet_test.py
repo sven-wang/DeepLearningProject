cd78c82bfbbb2c055119b3d3a2dd8ffd4a6c8bbd
WinttterZ <lwzhong@yahoo.com>
2018-04-26 18:52:21 -0400

    Commit

--- train_resnet_test.py
+++ train_resnet_test.py
@@ -142,7 +142,7 @@ def get_class_num():
 
 if __name__ == "__main__":
     # classes = get_class_num()
-    classes = 3476
+    classes = 834
     prev_state = None
     if len(sys.argv) == 2:
         prev_state = sys.argv[1]
