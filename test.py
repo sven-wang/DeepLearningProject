1a95c62d1cabd2f65ce771cb11ab724347fe6fe7
sven-wang <siyuanwang1123@gmail.com>
2018-05-04 12:07:25 -0400

    test trials done (not tested)

---
+++ test.py
@@ -0,0 +1,101 @@
+from resnet_2d_small import *
+import os
+import torch
+from sklearn.metrics import roc_curve, auc
+import numpy as np
+from scipy.spatial.distance import cosine
+
+
+class TestDataset(Dataset):
+    def __init__(self, filedir, enroldir, testdir):
+        self.dir = filedir
+        self.enrol_dir = enroldir
+        self.test_dir = testdir
+
+        self.triplets = []
+        with open(filedir, 'r') as csvfile:
+            for line in csvfile:
+                speaker_file, test_file, label = line.strip().split()
+                self.triplets.append((speaker_file, test_file, label))
+
+    def __getitem__(self, item):
+        speaker_file, test_file, label = self.triplets[item]
+        enrol = np.load(self.enrol_dir + speaker_file)  # eg. 32707 # todo: double check
+        test = np.load(self.test_dir + test_file)  # eg. tkwut_A
+        label = np.ones(1) if label == 'target' else np.zeros(1)
+
+        return to_tensor(enrol), to_tensor(test), to_tensor(label)
+
+    def __len__(self):
+        return len(self.triplets)
+
+
+def eer(y_gold, y_pred):
+    # y = [1, 1, 0, 0]
+    # y_pred = [0.5, 0.8, 0.5, 0.1]
+
+    fpr, tpr, threshold = roc_curve(y_gold, y_pred, pos_label=1)
+    fnr = 1 - tpr
+    eer_threshold = threshold[np.nanargmin(np.absolute((fnr - fpr)))]
+
+    EER = fpr[np.nanargmin(np.absolute((fnr - fpr)))]
+
+    print('eer_threshold:', eer_threshold)
+
+    return EER
+
+def to_variable(tensor):
+    # Tensor -> Variable (on GPU if possible)
+    if torch.cuda.is_available():
+        # Tensor -> GPU Tensor
+        tensor = tensor.cuda()
+    return torch.autograd.Variable(tensor)
+
+
+def test():
+
+    test_dataset = TestDataset("trials.txt", "enrol_features/", "test_features")  # todo: double check
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    # Load Model
+    model = DeepSpeakerModel(classes)
+    model.load_state_dict(torch.load(model_path))
+
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    # for EER
+    y_gold = []
+    y_pred = []
+
+    counter = 1
+    model.train()
+    for (data_a, data_p, label) in test_dataloader:
+        data_a, data_p = to_variable(data_a), to_variable(data_p)
+
+        # compute output
+        out_a, out_p = model(data_a)[1], model(data_p)[1]  # vector before the fc layer
+
+        # record similarity and true label for both pairs
+        np_a = out_a.data.cpu().numpy()
+        np_p = out_p.data.cpu().numpy()
+
+        for i in range(np_a.shape[0]):
+            y_gold.append(int(label.numpy()[0]))
+            y_pred.append(1 - cosine(np_a[i], np_p[i]))
+
+        if counter % 3000 == 0:
+            print('EER:', eer(y_gold, y_pred))
+        counter += 1
+
+    print('EER:', eer(y_gold, y_pred))
+
+
+if __name__ == "__main__":
+    batch_size = 1
+    classes = 1303
+
+    # todo: modify
+    model_path = 'best_state_small'
+
+    test()
