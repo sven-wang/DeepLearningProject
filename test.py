25a9e6e135923d6119e91b50f921e97227b1fce7
WinttterZ <lwzhong@yahoo.com>
2018-05-04 20:43:36 -0400

    Experiments

--- test.py
+++ test.py
@@ -7,23 +7,22 @@ from scipy.spatial.distance import cosine
 
 
 class TestDataset(Dataset):
-    def __init__(self, filedir, enroldir, testdir, file_map):
+    def __init__(self, filedir, enroldir, testdir, enrol_map, test_set):
         self.dir = filedir
         self.enrol_dir = enroldir
         self.test_dir = testdir
-        self.file_map = file_map
+        self.enrol_map = enrol_map
+        self.test_set = test_set
 
         self.pairs = []
         with open(filedir, 'r') as f:
             for line in f:
                 speaker_file, test_file, label = line.strip().split()
-                self.pairs.append((speaker_file, test_file, label))
+                if speaker_file in self.enrol_map and test_file in self.test_set:
+                    self.pairs.append((self.enrol_map[speaker_file], test_file + "-vad.npy", label))
 
     def __getitem__(self, item):
         speaker_file, test_file, label = self.pairs[item]
-
-        speaker_file = self.file_map[speaker_file]
-        test_file += "-vad.npy"
 
         enrol = np.load(self.enrol_dir + speaker_file)  # eg. 32707 # todo: double check
         test = np.load(self.test_dir + test_file)  # eg. tkwut_A
@@ -62,8 +61,13 @@ def test():
     with open("enrol_file_map.pickle", "rb") as handle:
         enrol_file_map = pickle.load(handle)
 
-    test_dataset = TestDataset("trials.txt", "enrol_features/", "test_features/", enrol_file_map)  # todo: double check
+    with open("test_file_set.pickle", "rb") as handle:
+        test_file_set = pickle.load(handle)
+
+    test_dataset = TestDataset("trials.txt", "enrol_features/", "test_features/", enrol_file_map, test_file_set)  # todo: double check
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    print(len(test_dataset))
 
     # Load Model
     model = DeepSpeakerModel(classes)
@@ -82,7 +86,7 @@ def test():
         data_a, data_p = to_variable(data_a), to_variable(data_p)
 
         # compute output
-        out_a, out_p = model(data_a)[1], model(data_p)[1]  # vector before the fc layer
+        out_a, out_p = model(data_a)[0], model(data_p)[0]  # vector before the fc layer
 
         # record similarity and true label for both pairs
         np_a = out_a.data.cpu().numpy()
@@ -104,6 +108,6 @@ if __name__ == "__main__":
     classes = 1303
 
     # todo: modify
-    model_path = 'best_state_small'
+    model_path = './experiments/triplet_loss/best_state_triplet_small'
 
     test()
